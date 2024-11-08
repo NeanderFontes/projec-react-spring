d1c88d20d0ab9d9a0261bbb64d0df1ce41c4633f
Neander Fontes <neanderffontes@gmail.com>
2024-11-08 18:40:46 +0000

    Criando categoria para produtos

---
+++ src/main/java/com/dev/backend/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.dev.backend.repository;
+
+import com.dev.backend.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}

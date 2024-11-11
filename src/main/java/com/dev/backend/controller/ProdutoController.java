fac0d8f71a2400bfd0cdb5296374a6830766d58c
Neander Fontes <neanderffontes@gmail.com>
2024-11-11 00:51:26 +0000

    Create products space

---
+++ src/main/java/com/dev/backend/controller/ProdutoController.java
@@ -0,0 +1,43 @@
+package com.dev.backend.controller;
+
+import com.dev.backend.model.Produto;
+import com.dev.backend.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping(value = "/",
+                produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Produto> listAll() {
+        return produtoService.listAll();
+    }
+
+    @PostMapping(value = "/",
+                produces = MediaType.APPLICATION_JSON_VALUE,
+                consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Produto criar(@RequestBody Produto criarProduto) {
+        return produtoService.criarProduto(criarProduto);
+    }
+
+    @PutMapping(value = "/",
+                produces = MediaType.APPLICATION_JSON_VALUE,
+                consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Produto atualizar(@RequestBody Produto atualizarProduto) {
+        return produtoService.atualizarProduto(atualizarProduto);
+    }
+
+    @DeleteMapping(value = "/")
+    public ResponseEntity<Void> eliminar(Long idParaEliminar) {
+        produtoService.elminarProduto(idParaEliminar);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/dev/backend/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.dev.backend.repository;
+
+import com.dev.backend.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}

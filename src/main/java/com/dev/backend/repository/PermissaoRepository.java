413d09234dd025e35b921579076bdbd5e2a4506c
Neander Fontes <neanderffontes@gmail.com>
2024-11-11 17:33:35 +0000

    Create permissao crud

---
+++ src/main/java/com/dev/backend/repository/PermissaoRepository.java
@@ -0,0 +1,8 @@
+package com.dev.backend.repository;
+
+import com.dev.backend.model.Permissao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+
+}
